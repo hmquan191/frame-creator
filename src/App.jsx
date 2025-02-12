830e82d597da239bae4575fa5a8557f74e751bdb
HMQ <hmquan191@gmail.com>
2025-02-12 15:04:35 +0700

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className=" bold flex items-center justify-center h-screen text-red-500">
+      App
+    </div>
+  );
+};
+
+export default App;
